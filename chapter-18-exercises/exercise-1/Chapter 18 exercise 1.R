d05b03311bdbb65cba0a10694e2687d2a35d40d3
Nahid Mohammed <nahid@Nahids-MacBook-Pro.local>
2020-02-13 15:32:56 -0800

    Week 6: Exercises

--- chapter-18-exercises/exercise-1/Chapter 18 exercise 1.R
+++ chapter-18-exercises/exercise-1/Chapter 18 exercise 1.R
@@ -4,13 +4,12 @@ library("jsonlite")
 
 # Be sure and check the README.md for complete instructions!
 
-
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R") # Go to that file, select all and hit ctrl-enter
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,26 +19,41 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json" # Where am I going to?
+uri <- paste0(base_uri, endpoint) 
 
+query_params_list <- list("api-key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
+response <- GET(uri, query = query_params_list)
+body <- fromJSON(content(response, "text"))
 
 
 # What kind of data structure did this produce? A data frame? A list?
-
+class(body) 
+is.data.frame(body)  
+is.list(body)  
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+names(body)
+names(body$results)
+View(body)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(body$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+review_one <- reviews[1, ]
+head_review <- first_review$headline
+summary <- first_review$summary_short
+link <- first_review$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+review <- list(headline=headline, summary=summary, link=link)
+
